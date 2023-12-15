07e5c7a44ae5accefbb2b885b232e94e323768e6
Korn Sriwattana <korn_s@cmu.ac.th>
2023-12-15 15:06:51 +0700

    editting lab9.2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 //Write the function printO() here
+double printO(int N,M){
+	if(N<=0 || M<=0){
+		cout << "Invalid input";
+	}else{
+
+	for(int i = 0; i<N; i++)
+		for(int j = 0; j<M; j++){
+			cout << "O";
+		}
+	cout << "\n";
+	}
+}
 
 int main(){
 	
